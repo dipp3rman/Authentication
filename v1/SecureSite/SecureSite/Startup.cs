5b0b4131b7608fcf7eaef54474307633222faa77
Mat Byrne <mat.byrne@psba.qld.gov.au>
2018-04-05 16:46:52 +1000

    Vanilla checkin - no changes

---
+++ v1/SecureSite/SecureSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecureSite.Startup))]
+namespace SecureSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
